353227335da359f7b9b2d1261e1b7969c915ac4e
kodam <hotsoup.h@gmail.com>
2013-06-23 02:34:14 +0900

    update map gen

--- src/map/main.js
+++ src/map/main.js
@@ -31,13 +31,34 @@ function mapGen(col,row) {
 		for(i=0;i<ids.length;i++){
 			ids[i] = i;
 		}
-		
+
 		var len = Math.floor(Math.random()*(num/6) + num/3);
+		ids.shuffle();
 		for(i=0;i<len;i++) ids.pop(); 
 		
 		return ids;
 	})(col*row);
-	
+
+	// gen room
+	for (i = 0; i < indexes.length; i++) {
+		//offset
+		var offset_x = Math.floor(Math.floor(indexes[i]%col)*10);
+		var offset_y = Math.floor(Math.floor(indexes[i]/col)*10);
+
+		var random_h = Math.floor(Math.random()*3 + 5);
+		var random_w = Math.floor(Math.random()*3 + 5);
+		var h = Math.floor(5 - Math.floor(random_h/2));
+		var w = Math.floor(5 - Math.floor(random_w/2));
+		offset_y = offset_y + h;
+		offset_x = offset_x + w;
+
+		for (j = offset_y; j < offset_y+random_h ; j++) {
+			for (k = offset_x; k < offset_x+random_w ; k++) {
+				map[j][k] = 132;
+			};
+		};
+	};
+
 	return map;
 }
 
@@ -48,7 +69,7 @@ function minimapGen(x,y,map){
 	var mini_map = new Group();
 	mini_map.x = mini_map.y = 40;
 	const WALL = map[0][0];
-	const T_SIZE = 5;
+	const T_SIZE = 4;
 
 	for (var i = 0; i < map.length; i++) {
 		for (var j = 0; j < map[i].length; j++) {
@@ -71,8 +92,8 @@ window.onload = function() {
     game.preload('chara1.png', 'map1.png');
     game.onload = function() {
         var map = new Map(16, 16);
-        var map_data = mapGen(5,3);
-        var mini_map = new minimapGen(5,3,map_data);
+        var map_data = mapGen(6,4);
+        var mini_map = new minimapGen(6,4,map_data);
 
         map.image = game.assets['map1.png'];
         map.loadData( map_data );
