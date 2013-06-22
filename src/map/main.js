1ce7f45509ee3da01aa416b34c0eb53f8e3462c9
kodam <hotsoup.h@gmail.com>
2013-06-23 00:05:08 +0900

    add generating mini map

--- src/map/main.js
+++ src/map/main.js
@@ -12,6 +12,9 @@ Array.prototype.shuffle = function() {
 	return this;
 }
 
+/*
+ * Generate Map
+ */
 function mapGen(col,row) {
 	// initialize map
 	var map = new Array(row*10);
@@ -38,15 +41,44 @@ function mapGen(col,row) {
 	return map;
 }
 
+/*
+ * Generate mini Map
+ */
+function minimapGen(x,y,map){
+	var mini_map = new Group();
+	mini_map.x = mini_map.y = 40;
+	const WALL = map[0][0];
+	const T_SIZE = 5;
+
+	for (var i = 0; i < map.length; i++) {
+		for (var j = 0; j < map[i].length; j++) {
+			if(map[i][j]==WALL){
+				var chip = new Sprite(T_SIZE,T_SIZE);
+				chip.width = chip.height = T_SIZE;
+				chip.x = j*T_SIZE;chip.y = i*T_SIZE;
+				chip.backgroundColor = 'rgba(10,10,120,0.5)';
+				mini_map.addChild(chip);
+			}
+		};
+	};
+
+	return mini_map;
+}
+
 window.onload = function() {
     var game = new Game(320, 320);
     game.fps = 20;
     game.preload('chara1.png', 'map1.png');
     game.onload = function() {
         var map = new Map(16, 16);
+        var map_data = mapGen(5,3);
+        var mini_map = new minimapGen(5,3,map_data);
+
         map.image = game.assets['map1.png'];
-        map.loadData( mapGen(5,3) );
+        map.loadData( map_data );
+
         game.rootScene.addChild(map);
+        game.rootScene.addChild(mini_map);
     };
     game.start();
 };
